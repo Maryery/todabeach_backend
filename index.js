c8e7369c71eb0b47a087f38a679bd02389d6eb61
Maryery Vargas Moreno <maryeryvargas@gmail.com>
2020-10-03 21:35:56 +0100

    create a function to calculate the best beach

--- index.js
+++ index.js
@@ -11,11 +11,48 @@ app.get('/', async (req, res) => {
 	const beaches = client.db('todabeach').collection('beach').find({});
 	const results = await beaches.toArray();
 
-	console.log(results[0]);
+	const bestBeachName = getBestBeachName(results);
+
+	const bestBeach = await client
+		.db('todabeach')
+		.collection('beach')
+		.findOne({ beach: bestBeachName });
+
 	res.setHeader('Content-Type', 'application/json');
-	res.end(JSON.stringify(results));
+	res.end(JSON.stringify(bestBeach));
 });
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
+
+function getBestBeachName(results) {
+	const points = results.map((beach) => {
+		let total = 0;
+
+		if (beach.hammocks == 'si') {
+			total += 33;
+		}
+
+		if (beach.temperature > 24 && beach.temperature < 30) {
+			total += 34;
+		} else if (beach.temperature >= 30 && beach.temperature < 36) {
+			total += 17;
+		}
+
+		if (beach.bathroom == 'si') {
+			total += 33;
+		}
+
+		return {
+			name: beach.beach,
+			points: total,
+		};
+	});
+
+	points.sort((a, b) => {
+		return b.points - a.points;
+	});
+
+	return points[0].name;
+}
